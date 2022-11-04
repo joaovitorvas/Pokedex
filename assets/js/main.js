cfec866f6c9bbc06293c728a75921b6aea5adeef
Joaovitorvas <joaovitorvasc.2307@outlook.com>
2022-11-04 08:58:40 -0300

    responsibidade botão mobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,12 +18,12 @@ function loadPokemonItems(offset, limit){
                 <ol class="types">
                     ${pokemon.types.map((type) => `<li class="type">${type}</li>`).join('')}
                 </ol>
-        
                 <img src="${pokemon.photo}" 
                 alt="${pokemon.name}">
             </div>
         </li> `)
         .join('')
+
 
         pokemonList.innerHTML += newHtml
         })
